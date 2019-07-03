9aab63fbe6d78d72648070991720c87eae181bd7
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2019-07-04 01:27:00 +0800

    Update terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,17 +1,17 @@
 terraform {
-  provider "aws" {
-    profile    = "default"
-    region     = "us-east-2"
-  }
-
   backend "s3" {
     encrypt = true
     bucket  = "udc-dev-cicd"
     region  = "us-east-2"
     key     = "terraform.udc-dev-cicd.tfstate"
   }
-
-  resource "aws_ecr_repository" "ecr-repo" {
-    name = "udc/python-template"
-  }
 }
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-2"
+}
+resource "aws_ecr_repository" "ecr-repo" {
+  name = "udc/python-template"
+}
+
