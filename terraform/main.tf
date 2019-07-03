b89993f97a7e9896caa8fb5a0d08e16951927735
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2019-07-04 01:25:58 +0800

    Setup backend for terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,17 @@
-provider "aws" {
-  profile    = "default"
-  region     = "us-east-2"
-}
+terraform {
+  provider "aws" {
+    profile    = "default"
+    region     = "us-east-2"
+  }
 
-resource "aws_ecr_repository" "ecr-repo" {
-  name = "udc/python-template"
-}
+  backend "s3" {
+    encrypt = true
+    bucket  = "udc-dev-cicd"
+    region  = "us-east-2"
+    key     = "terraform.udc-dev-cicd.tfstate"
+  }
 
+  resource "aws_ecr_repository" "ecr-repo" {
+    name = "udc/python-template"
+  }
+}
