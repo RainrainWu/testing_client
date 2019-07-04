9083790260011212f72e652704e8f5a291190aa4
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2019-07-04 12:38:03 +0800

    Fix fizzbuzz.py

--- app/fizzbuzz.py
+++ app/fizzbuzz.py
@@ -1,5 +1,5 @@
 def fizzbuzz(num: int) -> str:
-    fizz = "fizz" if num % 3 == 0 else ""
-    buzz = "buzz" if num % 5 == 0 else ""
+    fizz = 'fizz' if num % 3 == 0 else ''
+    buzz = 'buzz' if num % 5 == 0 else ''
     fizzbuz = fizz + buzz
     return fizzbuz if fizzbuz else str(num)
